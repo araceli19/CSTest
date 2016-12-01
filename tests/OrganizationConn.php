969890b5314de16490275cf213581a0e96032e52
araceli19 <agopar@csumb.edu>
2016-12-01 00:37:42 -0800

    added

---
+++ tests/OrganizationConn.php
@@ -0,0 +1,80 @@
+<?php
+ini_set('memory_limit', '256M');
+//will test the Volunteer Organization table
+
+include_once("Database.php"); //connects to the database
+class OrganizationConn{
+
+
+    public static $ID;
+    public static $Contact_name;
+    public static $Org_Name;
+    public static $Phone_Num;
+    public static $Email;
+    public static $Password;
+
+//testing begins with inserting data to Volunteer Organization
+    public static function insertDataOrg($Contact_name, $Org_Name, $Phone_Num, $Email, $Password)
+    {
+        $db = Database::setConnection();
+            $sql = "INSERT INTO Volunteer_Organization(Contact_name, Org_Name, Phone_Num, Email, Password)
+            VALUES('$Contact_name', '$Org_Name', '$Phone_Num', '$Email', '$Password');";
+
+
+            $val = $db->prepare($sql);
+
+            if($val->execute()){ //if it was executed
+                return true;
+            }
+            else{
+                return false;
+            }
+
+    }
+    //test removal of organizaion
+    public static function removeVolunteerOrg($Contact_name, $Org_Name, $Phone_Num, $Email, $Password){
+  $db = Database::setConnection();
+
+                $sql = "DELETE FROM Volunteer_Organization WHERE Contact_name = '$Contact_name'";
+
+                $val = $db->prepare($sql);
+                if($val->execute()){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+    }
+    //selects only one organizaion
+    public static function selectVolunteerOrg($Contact_name){
+
+      $db = Database::setConnection();
+
+
+        $sql = "SELECT Name FROM Volunteer_Organization WHERE Contact_name ='$Contact_name'";
+
+        $val = $db->prepare($sql);
+        $val->execute();
+        $retrieval = $val->fetch();
+
+        if($Contact_name == $retrieval['Contact_name']){
+
+            return $retrieval['Contact_name'];
+        }
+        else{
+            return "";
+        }
+    }
+    //retrieves all organization
+    public static function getAllOrg(){
+      $db = Database::setConnection();
+
+        $sql = "SELECT * FROM Volunteer_Organization";
+        $val = $db->prepare($sql);
+        $val->execute();
+        $retrieval = $val->fetchAll(PDO::FETCH_ASSOC);
+
+        return json_encode($retrieval);
+    }
+}
+?>
---
+++ tests/WebsiteOperatorConn.php
@@ -0,0 +1,81 @@
+
+<?php
+ini_set('memory_limit', '256M');
+//testing Website Operator, back en query connection
+//connect to database
+include_once("Database.php");
+class WebsiteOperatorConn{
+
+
+    public static $ID;
+    public static $Name;
+    public static $Phone_Num;
+    public static $Email;
+    public static $Password;
+
+
+//insert data, using query
+    public static function insertDataWebsiteOp($Name, $Phone_Num, $Email, $Password)
+    {
+        $db = Database::setConnection();
+            $sql = "INSERT INTO Website_Operator(Name,Phone_Num, Email, Password)
+            VALUES('$Name', '$Phone_Num', '$Email', '$Password');";
+
+
+            $val = $db->prepare($sql);
+
+            if($val->execute()){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+    }
+    //remove one operator
+    public static function removeWebsiteOp($Name, $Phone_Num, $Email, $Password){
+  $db = Database::setConnection();
+
+                $sql = "DELETE FROM Website_Operator WHERE Name = '$Name'";
+
+                $val = $db->prepare($sql);
+                if($val->execute()){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+    }
+    //select only one
+    public static function selectWebsiteOp($Name){
+
+      $db = Database::setConnection();
+
+
+        $sql = "SELECT Name FROM Website_Operator WHERE Name ='$Name'";
+
+        $val = $db->prepare($sql);
+        $val->execute();
+        $retrieval = $val->fetch();
+
+        if($Name == $retrieval['Name']){
+
+            return $retrieval['Name'];
+        }
+        else{
+            return "";
+        }
+    }
+    //retrieve all
+    public static function getAllWebsiteOp(){
+      $db = Database::setConnection();
+
+        $sql = "SELECT * FROM Website_Operator";
+        $val = $db->prepare($sql);
+        $val->execute();
+        $retrieval = $val->fetchAll(PDO::FETCH_ASSOC);
+
+        return json_encode($retrieval);
+    }
+}
+?>
